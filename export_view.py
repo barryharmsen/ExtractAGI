93083a67a906bc85e7819dd841e089fe738ef2b2
barryharmsen <barry@bitmetric.nl>
2017-03-19 22:44:48 +0100

    added scaling and mirroring
    Storing mirrored images and scaling

--- export_view.py
+++ export_view.py
@@ -1,7 +1,7 @@
 import struct
 import json
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 
 # Load configuration
 with open("config.json", "r") as infile:
@@ -13,6 +13,8 @@ palette = [(0, 0, 0), (0, 0, 160), (0, 255, 80), (0, 160, 160),
            (255, 80, 80), (255, 80, 255), (255, 255, 80), (255, 255, 255)]
 
 views = {}
+views["views"] = {}
+views["imageScale"] = config["imageScale"]
 
 with open(config["exportDir"]["main"] + 'dir.json') as dir_file:
     view_dir = json.load(dir_file)
@@ -32,7 +34,7 @@ with open(config["exportDir"]["main"] + 'dir.json') as dir_file:
             if signature == int('0x1234', 16):
 
                 # Empty dictionary for view
-                views[view_index] = {}
+                views["views"][view_index] = {}
                 # Vol no, not sure why we need this
                 vol = struct.unpack('B', v.read(1))[0]
                 # resource lenght, LO-HI
@@ -44,51 +46,54 @@ with open(config["exportDir"]["main"] + 'dir.json') as dir_file:
                 # Position of description
                 desc_pos = struct.unpack('<H', v.read(2))[0]
 
-                views[view_index]['no_of_loops'] = no_of_loops
-                views[view_index]['size'] = reslen
-                views[view_index]['vol'] = vol
-                views[view_index]['loops'] = {}
+                views["views"][view_index]['no_of_loops'] = no_of_loops
+                views["views"][view_index]['size'] = reslen
+                views["views"][view_index]['vol'] = vol
+                views["views"][view_index]['loops'] = {}
 
                 # Get loop positions
                 for i in range(0, no_of_loops):
                     # 5 = length of view header
                     loop_pos = struct.unpack('<H', v.read(2))[0] \
                                + view_offset + 5
-                    views[view_index]['loops'][i] = {}
-                    views[view_index]['loops'][i]['pos'] = loop_pos
+                    views["views"][view_index]['loops'][i] = {}
+                    views["views"][view_index]['loops'][i]['pos'] = loop_pos
 
                 # Get cells for each loop
-                for loop in views[view_index]['loops']:
-                    loop_offset = views[view_index]['loops'][loop]['pos']
+                for loop in views["views"][view_index]['loops']:
+                    loop_offset = views["views"][view_index]['loops'][loop]['pos']
                     v.seek(loop_offset, 0)
 
                     no_of_cells = struct.unpack('B', v.read(1))[0]
-                    views[view_index]['loops'][loop]['no_of_cells'] = no_of_cells
-                    views[view_index]['loops'][loop]['cells'] = {}
+                    views["views"][view_index]['loops'][loop]['no_of_cells'] = no_of_cells
+                    views["views"][view_index]['loops'][loop]['cells'] = {}
 
                     for i in range(0, no_of_cells):
 
                         loop_pos = struct.unpack('<H', v.read(2))[0] \
                                    + loop_offset
 
-                        views[view_index]['loops'][loop]['cells'][i] = {}
-                        views[view_index]['loops'][loop]['cells'][i]['pos'] = loop_pos
+                        views["views"][view_index]['loops'][loop]['cells'][i] = {}
+                        views["views"][view_index]['loops'][loop]['cells'][i]['pos'] = loop_pos
 
-                    for cell in views[view_index]['loops'][loop]['cells']:
-                        cell_offset = views[view_index]['loops'][loop]['cells'][cell]['pos']
+                    for cell in views["views"][view_index]['loops'][loop]['cells']:
+                        cell_offset = views["views"][view_index]['loops'][loop]['cells'][cell]['pos']
 
                         v.seek(cell_offset, 0)
                         cell_width = struct.unpack('B', v.read(1))[0]
                         cell_height = struct.unpack('B', v.read(1))[0]
                         cell_settings = struct.unpack('B', v.read(1))[0]
 
-                        cell_mirroring = cell_settings >> 4
+                        cell_mirrored = (cell_settings >> 4) > 0
+                        cell_mirrored_loop = (cell_settings >> 4) & 0b00000111
                         cell_transparency = cell_settings & 0b00001111
 
-                        views[view_index]['loops'][loop]['cells'][cell]['width'] = cell_width
-                        views[view_index]['loops'][loop]['cells'][cell]['height'] = cell_height
-                        views[view_index]['loops'][loop]['cells'][cell]['mirroring'] = cell_mirroring
-                        views[view_index]['loops'][loop]['cells'][cell]['transparency'] = cell_transparency
+                        views["views"][view_index]['loops'][loop]['cells'][cell]['width'] = cell_width
+                        views["views"][view_index]['loops'][loop]['cells'][cell]['height'] = cell_height
+                        views["views"][view_index]['loops'][loop]['cells'][cell]['transparency'] = cell_transparency
+                        views["views"][view_index]['loops'][loop]['cells'][cell]['mirrored'] = cell_mirrored
+                        if cell_mirrored and loop != cell_mirrored_loop:
+                            views["views"][view_index]['loops'][loop]['cells'][cell]['mirroredLoop'] = cell_mirrored_loop
 
                         # Add transparency
                         cell_transparent_rgb = palette[cell_transparency] + (0,)
@@ -123,15 +128,18 @@ with open(config["exportDir"]["main"] + 'dir.json') as dir_file:
 
                             col += (pixels * 2)
 
-
                         cell_filename = "%s_%s_%s.png" % (view_index, loop, cell)
                         img = Image.new('RGBA', ((cell_width * 2), cell_height))
                         img.putdata(cell_image)
                         img = img.resize((cell_width * 2 * config["imageScale"],
                                           cell_height * config["imageScale"]))
+
+                        # Flip image if it is mirrored from another loop
+                        if cell_mirrored and loop != cell_mirrored_loop:
+                            img = ImageOps.mirror(img)
                         img.save(config["exportDir"]["view"] + cell_filename, "PNG")
 
-                        views[view_index]['loops'][loop]['cells'][cell]['filename'] = cell_filename
+                        views["views"][view_index]['loops'][loop]['cells'][cell]['filename'] = cell_filename
 
 
 with open(config["exportDir"]["main"] + 'view.json', 'w') as outfile:
