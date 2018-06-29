43c3d50a691cf3d01db060e2a43900f9c0d016ba
Chad Armstrong <chad@edenwaith.com>
2018-06-28 21:25:44 -0600

    Clean up of Obj-C file.  Save contents to flat file and JSON file.

--- export_words_tok.m
+++ export_words_tok.m
@@ -11,177 +11,153 @@
  */
 
 #import <Foundation/Foundation.h>
-// #import <AppKit/AppKit.h>
 
-/*
-#include <sys/param.h>
-#include <sys/ucred.h>
-#include <sys/mount.h>
-*/
 #include <stdio.h>
-#include <string.h>
+
 
 int main(int argc, char *argv[]) 
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	NSFileManager *fm = [NSFileManager defaultManager];
+	NSMutableString *currentWord  = [[NSMutableString alloc] init];
+	NSMutableString *previousWord = [[NSMutableString alloc] init];
+	NSMutableDictionary *wordsDictionary = [[NSMutableDictionary alloc] init];
+		
+	FILE *fp, *fpout;
+	int data    = -1;
+	int ms_byte = -1;
+	int ls_byte = -1;
+	int initPos = -1;
 	
 	if (argc < 2) {
 		printf("usage: %s path/to/WORDS.TOK\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	
-	NSFileManager *fm = [NSFileManager defaultManager];
-	FILE *fp;
-	
 	if ((fp = fopen(argv[1], "r")) == NULL) {		
 		fprintf(stderr, "Could not open input file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	
-	int data = -1;
-	int msbyte = -1;
-	int lsbyte = -1;
-	int initPos = -1;
-	
-	fseek(fp, 1, SEEK_SET);
-	initPos = getc(fp);
-	printf("Data at position 1: %x %d", initPos, initPos);
-	
-	// exit(0); // temp code
+	if ((fpout = fopen("words.txt", "w")) == NULL) {
+		fprintf(stderr, "Could not open output file 'words.txt'\n");
+		exit(EXIT_FAILURE);
+	}
 	
 	// At the start of the file is a section that is always 26x2 bytes long. 
 	// This section contains a two byte entry for every letter of the alphabet. 
 	// It is essentially an index which gives the starting location of the words 
 	// beginning with the corresponding letter.
-	// fseek(fp, 52, SEEK_SET); // jump to position 52 in the file
 	
+	// Read byte 1 (second byte in the file, since the index starts at 0), to retrieve
+	// the offset where the words section begins.  Example value might be 3E, which is
+	// the hexadecimal value for 62.
+	fseek(fp, 1, SEEK_SET);
+	initPos = getc(fp);
+	printf("Data at position 1: %x (%d)", initPos, initPos);
+
+	// Jump to the offset for the first word.  Other examples might show jumping to offset
+	// 52, but that may cause other errors, so refer to the offset value read from byte 1.
 	fseek(fp, initPos, SEEK_SET);
+		
+
 	
-	// Get the indexes of the words, which are stored in the first 52 bytes
+	while (true) { // outer loop
 	
-	printf("Storage size for int is %d and short is %d \n", sizeof(int), sizeof(short));
-	
-	short buffer[2];
-	short short_data = -1;
-	short buf[2];
-	
-	/*
-	// https://cboard.cprogramming.com/c-programming/84251-read-2-bytes-file.html
-	for (int i = 0; i < 26; i++) {
-		if (fread(buf, sizeof(short), 1, fp) == 1)
-   		{
-      		// printf("%02x %02x\n", (int) buf[0], (int) buf[1]);
-      		printf("%02x (%d)\n", buf[0], buf[0]);
-   		}
-	
-//  		fread((void *)buffer, sizeof(short), 1, fp);
-//  		printf("%d:: buffer: %s (%x)\n", i, buffer, buffer);
- 		
-//		short_data = getc(fp);
-//		printf("%d) %d (%x)\n", i, short_data, short_data);
-	}
-	
-	*/
-	
-	// data = getc(fp);
-	
-	// For position 54, the number 49 was returned, when printed as a character is
-	// the number 1 in decimal.
-	// If the number is 0, that is a nul, which might represent the nul character, as well
-	// printf("data:: %d %c\n", data, data); 
-	
-	printf("\n-------------------------------------\n");
-	
-	// char new_word[80]; //  = "";
-	NSMutableString *currentWord  = [[NSMutableString alloc] init];
-	NSMutableString *previousWord = [[NSMutableString alloc] init];
-	
-	bool outer_loop = true;
-	
-	int loop_counter = 0;
-	
-	// while (( data = getc(fp)) != EOF) {
-	while (true) {
-	
-//		NSLog(@"1. --- Prev: %@ | Curr: %@ ---", previousWord, currentWord);
 		[previousWord setString: currentWord];
-//		NSLog(@"2. --- Prev: %@ | Curr: %@ ---", previousWord, currentWord);
 		[currentWord setString: @""];
-//		NSLog(@"3. --- Prev: %@ | Curr: %@ ---", previousWord, currentWord);
 	
 		if (( data = getc(fp)) == EOF) {
-			// break out of the outer loop
+			// Once the end of file has been reached, break out of the outer loop
 			break;
 		}
 	
-		// Need to copy some portion of the previous word to the current word
-		// printf("string length: %d %c\n", data, data);
+		// Need to copy a given substring of the previous word to the current word
 		if (data <= [previousWord length]) {
 			[currentWord setString: [previousWord substringToIndex: data]];
-			// NSLog(@"previousWord substring
 		}
-		NSLog(@"currentWord after copying substring %@ of length %d", currentWord, data);
-//	data = getc(fp);
 		
-		while (true) {
+		while (true) { // inner loop
 		
 			if (( data = getc(fp)) == EOF) {
+				// If the end of file has been reached, break out of the inner loop
 				break;
 			}
 		
+			// The only values of interest are under 32, 95, or over 127.  
+			// Ignore any values from 32 through 127.
 			if (data < 32) {
+				// A letter of the current word.
 				int new_data = data ^ 127;
-				// printf("new_data:: %d %c\n", new_data, new_data);
-				// printf("%c ", new_data); // Useful for seeing what letter is being added
-				
-// 				if (new_data == '~') {
-// 					printf("new_data is now ~ (%d)", data);
-// 				}
 				[currentWord appendFormat:@"%c", new_data];
-				// strcpy(new_word, (char)new_data);
+
+			} else if (data == 95) {
+				// A space character
+				[currentWord appendString: @" "];
+				
 			} else if (data > 127) {
+			
 				int new_data = (data - 128) ^ 127;
-				int other_data = data - 128; // data - 0x80
-				// printf("new_data:: %d %c\n", new_data, new_data);
-				// printf("%c [%c]", new_data, other_data);
-				// strcpy(new_word, (char)new_data);
-				// printf("new_word: %s", new_word);
 				[currentWord appendFormat:@"%c", new_data];
 				NSLog(@"currentWord: %@", currentWord);
-				// [currentWord setString: @""];
-				// memset(new_word, 0, strlen(new_word));
-				break;
-			} else if (data == 95) {
-				// printf("   _data:: %d  %c\n", data, data);
-				printf("%c ", data);
-				[currentWord appendString: @" "];
-			} else {
-				//printf("    data:: %d %c\n", data, data);
-				printf("**%c** ", data);
+				NSString *stringToWrite = [NSString stringWithFormat: @"%@\n", currentWord];
+				fputs([stringToWrite cStringUsingEncoding: NSASCIIStringEncoding], fpout);
+
+				break; // break out of the inner loop 
+				
 			}
 		}
 		
-		// Get two more bytes of data
-		msbyte = getc(fp);
-		lsbyte = getc(fp);
+		// Get two more bytes of data to determine how to group the words together
+		ms_byte = getc(fp);
+		ls_byte = getc(fp);
 		
-		int word_block_num = msbyte*256 + lsbyte;
+		// Determine the word group index
+		int word_block_num = ms_byte*256 + ls_byte;
 		
-		printf("msbyte: %d lsbyte: %d -> %d ---------\n", msbyte, lsbyte, word_block_num);
 		
-		// Test code to limit the number of loops
-// 		loop_counter++;
-// 		if (loop_counter > 3) {
-// 			break;
-// 		}
+		// printf("ms_byte: %d ls_byte: %d -> %d ---------\n", ms_byte, ls_byte, word_block_num);
 		
-// 		read(wordstok,msbyte); { read a byte value }
-//     	read(wordstok,lsbyte); { read another byte value }
-//     	wordblocknum := msbyte*256 + lsbyte;
+		if (word_block_num >= 0) {
+		
+			NSString *key = [NSString stringWithFormat:@"%d", word_block_num]; // NSDictionary keys must be objects not value types
+			NSString *newWord = [currentWord copy]; // Copy the current word to a new string
+			
+			// Add word to dictionary
+			if ([wordsDictionary objectForKey: key]) {
+				// If the key exists, add the new word to the existing value array
+				NSMutableArray *wordsArray = [[NSMutableArray alloc] initWithArray: [wordsDictionary objectForKey: key]];
+				[wordsArray addObject: newWord];
+				[wordsDictionary setObject: wordsArray forKey: key];
+			} else {
+				// If the key does not exist yet, add the current word in an array				
+				[wordsDictionary setObject: @[newWord] forKey: key];
+			}
+		}
+		
 	}
 	
+	// Write dictionary contents to a file (JSON format)
+	NSError *error = nil; 
+	NSData *jsonData = [NSJSONSerialization dataWithJSONObject:wordsDictionary 
+                                                   options:NSJSONWritingPrettyPrinted | NSJSONWritingSortedKeys 
+                                                     error:&error];
+                                                     
+    if (error != nil) {
+    	NSLog(@"Error creating JSON data: %@", [error localizedDescription]);
+    } else {
+    	NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+
+		BOOL succeed = [jsonString writeToFile:@"words.json" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+		if (succeed == NO) {
+			NSLog(@"Error saving file 'words.json': %@", [error localizedDescription]);
+		}
+    }
+
 	
 	fclose(fp);
+	fclose(fpout);
 	
 	[pool drain];
 	return 0;
