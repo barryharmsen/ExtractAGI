99175cf6d5306aa2a5eb9c059132e84f6f298bea
Chad Armstrong <chad@edenwaith.com>
2018-07-06 20:38:13 -0600

    First working version of export_dir.m with executable and JSON output.

--- export_dir.m
+++ export_dir.m
@@ -22,24 +22,88 @@ int main(int argc, char *argv[])
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	
+	NSArray *fileList = @[@"VIEWDIR", @"PICDIR", @"LOGDIR", @"SNDDIR"]; // list of the 4 "DIR" files
+	NSMutableDictionary *directories = [NSMutableDictionary new];
+	NSFileManager *fm = [NSFileManager defaultManager];
 	NSString *sourceDir = nil;
 	
 	// Set the source directory path.  If one hasn't been specified, assume the cwd
-	if (argc >= 2) {
-		sourceDir = [NSString stringWithUTF8String:argv[1]];
-	} else {
+	if (argc < 2) {
 		sourceDir = @"./";
+	} else {
+		sourceDir = [NSString stringWithUTF8String:argv[1]];
 	}
-	
-	NSLog(@"sourceDir:: %@", sourceDir);
-	
-	NSArray *fileList = @[@"VIEWDIR", @"PICDIR", @"LOGDIR", @"SNDDIR"];
-	NSMutableDictionary *directories = [NSMutableDictionary new];
 	
 	for (NSString *fileName in fileList) {
 		
-		NSString *dirFilePath = [sourceDir stringByAppendingPathComponent: fileName];
-		NSLog(@"dirFilePath:: %@", dirFilePath);
+		NSError *error = nil;
+		NSString *dirFilePath = [[sourceDir stringByAppendingPathComponent: fileName] stringByExpandingTildeInPath];
+
+		if ([fm fileExistsAtPath: dirFilePath] == YES) {
+
+			NSDictionary *fileAttr = [fm attributesOfItemAtPath: dirFilePath error: &error];
+		
+			if (error != nil) {
+				NSLog(@"Error:: %@", [error localizedDescription]);
+			} else {
+			
+				FILE *fp;
+				NSMutableDictionary *directoryContents = [NSMutableDictionary new];
+				double fileSize = [[fileAttr objectForKey:NSFileSize] doubleValue];
+				
+				// fileSize should be evenly divisible by 3
+				int numDirEntries = fileSize / 3; // get the number of dir entries
+				
+				// Open up the file at the path dirFilePath
+				if ((fp = fopen([dirFilePath UTF8String], "r")) == NULL) {		
+					fprintf(stderr, "Could not open input file %s\n", [dirFilePath UTF8String]);
+					break;
+				}
+				
+				for (int i = 0; i < numDirEntries; i++) {
+					int byte1 = getc(fp);
+					int byte2 = getc(fp);
+					int byte3 = getc(fp);
+					
+					if (byte1 != 255 && byte2 != 255 && byte3 != 255) {
+						
+						int vol = (byte1 & 0b11110000) >> 4; // same as (byte1 & 240) >> 4
+						int offset = (byte1 & 0b00001111) << 16; // (byte1 & 15) << 16
+						offset += (byte2 << 8);
+						offset += byte3;
+						
+						NSString *key = [NSString stringWithFormat:@"%d", i];
+						NSDictionary *dict = @{@"vol": [NSNumber numberWithInt: vol], 
+											   @"offset": [NSNumber numberWithInt: offset]};
+						[directoryContents setObject: dict forKey: key];
+						
+					}
+				}
+				
+				[directories setObject: directoryContents forKey: fileName];
+				
+				fclose(fp);
+			}
+		} else {
+			NSLog(@"Error: File does not exist at %@", dirFilePath);
+		}
+	}
+	
+	// Write dictionary contents to a file (JSON format)
+	NSError *error = nil; 
+	NSData *jsonData = [NSJSONSerialization dataWithJSONObject:directories 
+                                                   options:NSJSONWritingPrettyPrinted | NSJSONWritingSortedKeys 
+                                                     error:&error];
+                                                     
+	if (error != nil) {
+		NSLog(@"Error creating JSON data: %@", [error localizedDescription]);
+	} else {
+		NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+
+		BOOL succeed = [jsonString writeToFile:@"dir.json" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+		if (succeed == NO) {
+			NSLog(@"Error saving file 'words.json': %@", [error localizedDescription]);
+		}
 	}
 	
 	[pool drain];
